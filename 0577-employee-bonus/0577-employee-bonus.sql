331b5316e5f7a14280b10f4c377cb019c89ba6a9
Vinayak Shivanagutti <129957615+Vinayak2712@users.noreply.github.com>
2024-08-11 09:26:38 +0530

    Update 0577-employee-bonus.sql

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -4,6 +4,3 @@ FROM Employee a
 LEFT JOIN Bonus b ON a.empId = b.empId
 WHERE bonus < 1000 OR Bonus IS NULL;
 
--- SELECT Employee.name,Bonus.bonus FROM Employee 
--- LEFT JOIN Bonus ON Employee.empID = Bonus.empID
--- WHERE bonus < 1000 OR Bonus IS NULL ;
